cf06825f48338225840e31ac2fe0d2d41e3d0a2c
rijojohn85 <rijojohn85@gmail.com>
2024-08-22 08:54:42 +0530

    almost there

--- rwc/rwc.c
+++ rwc/rwc.c
@@ -4,45 +4,50 @@
 
 const char *argp_program_bugaddress = "rijojohn85@gmail.com";
 const char *argp_program_version = "rwc - 0.0.1";
-static char doc[] = "char doc";
-static char args_doc[] = "Arguments accepted";
+static char doc[] = "wc - print newline, word and byte counts for each file";
+static char args_doc[] = "wc [OPTION] [FILE]";
 
 /*The options we understand. */
 static struct argp_option options[] = {
-    {"verbose", 'v', 0, 0, "Produce verbose output"},
-    {"quiet", 'q', 0, 0, "Don't produce any output"},
-    {"silent", 's', 0, OPTION_ALIAS},
-    {"output", 'o', "FILE", 0, "Output to FILE instead of standard output"},
+    {"bytes", 'c', 0, 0, "print the byte counts"},
+    {"chars", 'm', 0, 0, "print the character counts"},
+    {"lines", 'l', 0, 0, "print the new line counts"},
+    {"max-line-length", 'L', 0, 0,
+     "print the length of the longest line(wrt to chars)"},
+    {"word", 'w', 0, 0, "print the word counts"},
     {0}};
 
 struct arguments {
-  char *args[2];
-  int silent, verbose;
-  char *output_file;
+  int bytes, chars, lines, max_line_length, word, no_files;
+  char **files;
 };
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
   struct arguments *arguments = state->input;
   switch (key) {
-  case 'q':
-  case 's':
-    arguments->silent = 1;
+  case 'c':
+    arguments->bytes = 1;
     break;
-  case 'v':
-    arguments->verbose = 1;
+  case 'm':
+    arguments->chars = 1;
     break;
-  case 'o':
-    arguments->output_file = arg;
+  case 'l':
+    arguments->lines = 1;
+    break;
+  case 'L':
+    arguments->max_line_length = 1;
+    break;
+  case 'w':
+    arguments->word = 1;
     break;
   case ARGP_KEY_ARG:
-    if (state->arg_num >= 2) {
-      printf("lots of args/n");
-      argp_usage(state);
-    }
-    arguments->args[state->arg_num] = arg;
+    // TODO:
+    printf("argnum: %d\n", state->next);
+    arguments->files = &state->argv[state->arg_num];
+    state->next = state->argc;
     break;
   case ARGP_KEY_END:
-    if (state->arg_num < 2) {
-      printf("few of args/n");
+    arguments->no_files = state->arg_num;
+    if (state->arg_num < 1) {
       argp_usage(state);
     }
     break;
@@ -57,18 +62,30 @@ int main(int argc, char **argv) {
   struct arguments arguments;
 
   /* Default values. */
-  arguments.silent = 0;
-  arguments.verbose = 0;
-  arguments.output_file = "-";
+  arguments.bytes = 0;
+  arguments.chars = 0;
+  arguments.lines = 0;
+  arguments.max_line_length = 0;
+  arguments.word = 0;
 
   /* Parse our arguments; every option seen by parse_opt will
      be reflected in arguments. */
   argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
-  printf("ARG1 = %s\nARG2 = %s\nOUTPUT_FILE = %s\n"
-         "VERBOSE = %s\nSILENT = %s\n",
-         arguments.args[0], arguments.args[1], arguments.output_file,
-         arguments.verbose ? "yes" : "no", arguments.silent ? "yes" : "no");
+  // printf("ARG1 = %s\nARG2 = %s\nOUTPUT_FILE = %s\n"
+  //        "VERBOSE = %s\nSILENT = %s\n",
+  //        arguments.args[0], arguments.args[1], arguments.output_file,
+  //        arguments.verbose ? "yes" : "no", arguments.silent ? "yes" : "no");
+  printf("Files: ");
+  for (int i = 0; arguments.files[i]; i++) {
+    printf("%s ", arguments.files[i]);
+  }
+  printf("\n");
+  printf("BYTES: %s\nCHARS: %s\nLINES: %s\nMAX-LINE-LENGTH: %s\nWORDS: %s\n",
+         arguments.bytes ? "yes" : "no", arguments.chars ? "yes" : "no",
+         arguments.lines ? "yes" : "no",
+         arguments.max_line_length ? "yes" : "no",
+         arguments.word ? "yes" : "no");
 
   exit(0);
 }
