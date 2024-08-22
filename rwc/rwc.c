0fbf79dfe07c1fc21b4d01a92b59957569f7a2c6
rijojohn85 <rijojohn85@gmail.com>
2024-08-22 06:29:39 +0530

    sort of working now

--- rwc/rwc.c
+++ rwc/rwc.c
@@ -26,44 +26,49 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
   case 'q':
   case 's':
     arguments->silent = 1;
-    printf("q chosen\n");
     break;
   case 'v':
     arguments->verbose = 1;
-    printf("v chosen\n");
     break;
   case 'o':
-    printf("file: %s\n", arg);
     arguments->output_file = arg;
     break;
   case ARGP_KEY_ARG:
     if (state->arg_num >= 2) {
+      printf("lots of args/n");
       argp_usage(state);
     }
     arguments->args[state->arg_num] = arg;
     break;
   case ARGP_KEY_END:
     if (state->arg_num < 2) {
+      printf("few of args/n");
       argp_usage(state);
     }
     break;
   default:
-    printf("unknown\n");
     return ARGP_ERR_UNKNOWN;
   }
   return 0;
 }
 static struct argp argp = {options, parse_opt, args_doc, doc};
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
   struct arguments arguments;
+
+  /* Default values. */
   arguments.silent = 0;
   arguments.verbose = 0;
   arguments.output_file = "-";
+
+  /* Parse our arguments; every option seen by parse_opt will
+     be reflected in arguments. */
   argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
   printf("ARG1 = %s\nARG2 = %s\nOUTPUT_FILE = %s\n"
          "VERBOSE = %s\nSILENT = %s\n",
          arguments.args[0], arguments.args[1], arguments.output_file,
          arguments.verbose ? "yes" : "no", arguments.silent ? "yes" : "no");
-  return EXIT_SUCCESS;
+
+  exit(0);
 }
